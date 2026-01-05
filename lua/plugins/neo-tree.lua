a566197734683ea467bd0cd689a76eabe98512a5
marvin.weiss <marvin.weiss@stud.th-luebeck.de>
2026-01-05 12:39:07 +0100

    Add: autoclose on file open

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,24 +1,33 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-    "nvim-tree/nvim-web-devicons",
-  },
-  lazy = false,
-  
-  keys = {
-    { "<C-n>", ":Neotree filesystem toggle left<CR>", desc = "Toggle Neo-tree" },
-  },
-  
-  opts = {
-    filesystem = {
-      filtered_items = {
-        visible = true,
-        hide_dotfiles = false,
-        hide_gitignored = true,
-      },
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "MunifTanjim/nui.nvim",
+        "nvim-tree/nvim-web-devicons",
     },
-  },
+    lazy = false,
+
+    keys = {
+        { "<C-n>", ":Neotree filesystem toggle left<CR>", desc = "Toggle Neo-tree" },
+    },
+
+    opts = {
+        filesystem = {
+            filtered_items = {
+                visible = true,
+                hide_dotfiles = false,
+                hide_gitignored = true,
+            },
+        },
+        event_handlers = {
+            {
+                event = "file_opened",
+                handler = function(file_path)
+                    -- auto close
+                    require("neo-tree.command").execute({ action = "close" })
+                end
+            },
+        },
+    },  
 }
